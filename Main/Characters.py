f9b9d76c354eec19a02103b51de94f4a3ef1ab32
ibasow1015 <32226007+ibasow1015@users.noreply.github.com>
2017-11-12 03:22:53 -0500

    fuck you nigel

--- Main/Characters.py
+++ Main/Characters.py
@@ -7,37 +7,52 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, name):
         pygame.sprite.Sprite.__init__(self)
         self.dest = [x, y]
+        self.fireDest = [x, y]
         self.gold = 200
         self.animationState = 0
+        self.fireState = 0
         self.animationDirection = 'right'
         self.movementState = 'still'
+        self.fireOn = 'off'
 
     def move(self, epsilon=6):
         # location command
         destX, destY = self.dest[0], self.dest[1]
+        fdestX, fdestY = self.fireDest[0], self.fireDest[1]
         # current location
         x, y = self.rect.center[0], self.rect.center[1]
+        fx, fy = self.fireRect.center[0], self.fireRect.center[1]
         # distance to travel
         dx = destX - x
         dy = destY - y
+        fdx = fdestX - fx
+        fdy = fdestY - fy
+
         # placeholder for direction
         xDir, yDir = 1, 1
+        fxDir, fyDir = 1, 1
         if (dx < 0):
             xDir = -1
+        if (fdx < 0):
+            fxDir = -1
         if (dy < 0):
             yDir = -1
+        if (fdy < 0):
+            fyDir = -1
             # do nothing if character is within range to prevent spazzing
         if (dy < epsilon and dy > -epsilon and dx < epsilon and dx > -epsilon):
             self.rect.center = (x, y)
             self.movementState = 'still'
         elif (dy < epsilon and dy > -epsilon):
             self.rect.center = (x + self.speed * xDir, y)
+            self.fireRect.center = (fx + self.speed * xDir, fy)
             if (xDir > 0):
                 self.animationDirection = 'right'
             else:
                 self.animationDirection = 'left'
         elif (dx < epsilon and dx > -epsilon):
             self.rect.center = (x, y + self.speed * yDir)
+            self.fireRect.center = (fx, fy + self.speed * yDir)
             if (xDir > 0):
                 self.animationDirection = 'right'
             else:
@@ -49,6 +64,7 @@ class Player(pygame.sprite.Sprite):
             i = self.speed * math.cos(theta) * xDir
             j = self.speed * math.sin(theta) * yDir
             self.rect.center = (x + i, y + j)
+            self.fireRect.center = (fx + i, fy + j)
             self.movementState = 'moving'
             if (abs(i) > abs(j) or yDir == 1):
                 if (xDir > 0):
@@ -58,7 +74,48 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.animationDirection = 'up'
 
-    def update(self):
+        if (fdy < epsilon and fdy > -epsilon and fdx < epsilon and fdx > -epsilon):
+            self.fireRect.center = (fx, fy)
+            self.fireOn = 'off'
+        elif (fdy < epsilon and fdy > -epsilon):
+            self.fireRect.center = (fx + self.speed * fxDir, fy)
+            if (fxDir > 0):
+                self.animationDirection = 'right'
+            else:
+                self.animationDirection = 'left'
+        elif (fdx < epsilon and fdx > -epsilon):
+            self.fireRect.center = (fx, fy + self.speed * fyDir)
+            if (fxDir > 0):
+                self.animationDirection = 'right'
+            else:
+                self.animationDirection = 'left'
+        else:
+            # get vector angle
+            theta = abs(math.atan(fdy / fdx))
+            # calculate unit vector
+            i = self.speed * math.cos(theta) * fxDir
+            j = self.speed * math.sin(theta) * fyDir
+            self.fireRect.center = (fx + i, fy + j)
+            self.movementState = 'moving'
+            if (abs(i) > abs(j) or fyDir == 1):
+                if (fxDir > 0):
+                    self.animationDirection = 'right'
+                else:
+                    self.animationDirection = 'left'
+            else:
+                self.animationDirection = 'up'
+
+    def drawFire(self, display):
+        self.ability3Move()
+
+        x, y = self.fireRect[0], self.fireRect[1]
+
+        display.blit(self.fireImage, (x, y))
+
+    def update(self, display):
+        if (self.fireOn == 'on'):
+            self.drawFire(display)
+
         self.move(15)
         self.animateWalk(self.animationDirection)
         self.health += self.regen
@@ -68,6 +125,8 @@ class Player(pygame.sprite.Sprite):
         if (self.health > self.maxHealth):
             self.health = self.maxHealth
         self.abilityTimers()
+
+
 
     def setX(self, x):
         self.rect.x += x
@@ -91,5 +150,9 @@ def initCharacter(data):
 
 
 def drawCharacter(display, data):
-    data.players.update()
+    data.players.update(display)
     data.players.draw(display)
+
+
+
+
