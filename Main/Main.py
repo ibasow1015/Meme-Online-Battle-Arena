8fd9b83dd5890cdaaf73e1bf164ff059b7957281
ibasow1015 <32226007+ibasow1015@users.noreply.github.com>
2017-11-11 21:07:47 -0500

    fuck you nigel

--- Main/Main.py
+++ Main/Main.py
@@ -8,24 +8,24 @@ import drawMap
 
 
 def init(data):
-    data.unit = data.width / 100
-    data.players = pygame.sprite.Group()
-    Characters.initCharacter(data)
-    data.minions = Minions.Minions()
-    data.minion = Minions.Minion((100, 30), data)
-    data.minions.add(data.minion)
-    data.timer = 0
-    icons.initIcons(data)
-    data.scrollX = data.scrollY = 0
+	data.unit = data.width / 100
+	data.players = pygame.sprite.Group()
+	Characters.initCharacter(data)
+	data.minions = Minions.Minions()
+	data.minion = Minions.Minion((100, 30), data)
+	data.minions.add(data.minion)
+	data.timer = 0
+	icons.initIcons(data)
+	data.scrollX = data.scrollY = 0
 
 
 def mouseDown(event, data):
-    if (event.button == 3):
-        data.player.dest = event.pos
+	if (event.button == 3):
+		data.player.dest = event.pos
 
 
 def mouseUp(event, data):
-    pass
+	pass
 
 
 def keyDown(event, data):
@@ -79,86 +79,89 @@ def keyDown(event, data):
 
 
 def keyUp(event, data):
-    if (event.key == 273):
-        data.downPressed = False
+	if (event.key == 273):
+		data.downPressed = False
 
 
 def timerFired(data):
-    data.timer += 250
-    data.minions.update(data.timer, data)
-    pass
+	data.timer += 250
+	data.minions.update(data.timer, data)
+	pass
 
 
 def redrawAll(display, data):
-    drawMap.drawMap(data, display)
-    Characters.drawCharacter(display, data)
-    data.minions.drawMinions(display)
-    UI.drawTaskbar(display, data)
-    icons.drawIcons(display, data)
+	drawMap.drawBoard(data, display)
+	drawMap.drawMap(data, display)
+	Characters.drawCharacter(display, data)
+	data.minions.drawMinions(display)
+	UI.drawTaskbar(display, data)
+	icons.drawIcons(display, data)
+
+	pygame.display.flip()
 
 
 def run(width=300, height=300):
-    def redrawAllWrapper(display, data):
-        display.fill((255, 255, 255))
-        redrawAll(display, data)
-        pygame.display.update()
+	def redrawAllWrapper(display, data):
+		display.fill((255, 255, 255))
+		redrawAll(display, data)
+		pygame.display.update()
 
-    def mouseDownWrapper(event, display, data):
-        mouseDown(event, data)
-        redrawAllWrapper(display, data)
+	def mouseDownWrapper(event, display, data):
+		mouseDown(event, data)
+		redrawAllWrapper(display, data)
 
-    def mouseUpWrapper(event, display, data):
-        mouseUp(event, data)
-        redrawAllWrapper(display, data)
+	def mouseUpWrapper(event, display, data):
+		mouseUp(event, data)
+		redrawAllWrapper(display, data)
 
-    def keyDownWrapper(event, display, data):
-        keyDown(event, data)
-        redrawAllWrapper(display, data)
+	def keyDownWrapper(event, display, data):
+		keyDown(event, data)
+		redrawAllWrapper(display, data)
 
-    def keyUpWrapper(event, display, data):
-        keyUp(event, data)
-        redrawAllWrapper(display, data)
+	def keyUpWrapper(event, display, data):
+		keyUp(event, data)
+		redrawAllWrapper(display, data)
 
-    def quit():
-        pygame.quit()
-        sys.exit()
+	def quit():
+		pygame.quit()
+		sys.exit()
 
-    def timerFiredWrapper(display, data):
-        timerFired(data)
-        redrawAllWrapper(display, data)
-        data.fpsClock.tick(data.fps)
+	def timerFiredWrapper(display, data):
+		timerFired(data)
+		redrawAllWrapper(display, data)
+		data.fpsClock.tick(data.fps)
 
-    # Set up data and call init
-    class Struct(object):
-        pass
+	# Set up data and call init
+	class Struct(object):
+		pass
 
-    data = Struct()
-    data.width = width
-    data.height = height
-    data.fps = 30  # frames per second
-    data.fpsClock = pygame.time.Clock()
-    init(data)
+	data = Struct()
+	data.width = width
+	data.height = height
+	data.fps = 30  # frames per second
+	data.fpsClock = pygame.time.Clock()
+	init(data)
 
-    # initialize module and display
-    pygame.init()
-    data.font = pygame.font.SysFont("helvetica", 15)
-    display = pygame.display.set_mode((data.width, data.height))
-    pygame.display.set_caption('RTS')
+	# initialize module and display
+	pygame.init()
+	data.font = pygame.font.SysFont("helvetica", 15)
+	display = pygame.display.set_mode((data.width, data.height))
+	pygame.display.set_caption('RTS')
 
-    # main loop
-    while (True):
-        for event in pygame.event.get():
-            if (event.type == QUIT):
-                quit()
-            if (event.type == KEYDOWN):
-                keyDownWrapper(event, display, data)
-            if (event.type == KEYUP):
-                keyUpWrapper(event, display, data)
-            if (event.type == MOUSEBUTTONDOWN):
-                mouseDownWrapper(event, display, data)
-            if (event.type == MOUSEBUTTONUP):
-                mouseUpWrapper(event, display, data)
-        timerFiredWrapper(display, data)
+	# main loop
+	while (True):
+		for event in pygame.event.get():
+			if (event.type == QUIT):
+				quit()
+			if (event.type == KEYDOWN):
+				keyDownWrapper(event, display, data)
+			if (event.type == KEYUP):
+				keyUpWrapper(event, display, data)
+			if (event.type == MOUSEBUTTONDOWN):
+				mouseDownWrapper(event, display, data)
+			if (event.type == MOUSEBUTTONUP):
+				mouseUpWrapper(event, display, data)
+		timerFiredWrapper(display, data)
 
 
 run(600, 600)
--- Main/drawMap.py
+++ Main/drawMap.py
@@ -2,82 +2,106 @@ import pygame
 
 
 def drawMap(data, display):
-    # map dimensions
-    data.mapWidth = 7000
-    data.mapHeight = 7000
+	# map dimensions
+	data.mapWidth = 7000
+	data.mapHeight = 7000
 
-    height = data.mapHeight
-    width = data.mapWidth
+	height = data.mapHeight
+	width = data.mapWidth
 
-    # when use arrow keys
-    data.mapStep = 200
+	# when use arrow keys
+	data.mapStep = 200
 
-    sx = data.scrollX
-    sy = data.scrollY
+	sx = data.scrollX
+	sy = data.scrollY
 
-    margin = 100
-    baseRad = height * 1 // 5
-    towerRad = height // 100
+	margin = 100
+	baseRad = height * 1 // 5
+	towerRad = height // 100
 
-    tower1Color = (0, 0, 255)
-    tower2Color = (255, 0, 0)
+	tower1Color = (0, 0, 255)
+	tower2Color = (255, 0, 0)
 
-    # Draw the whole map
-    pygame.draw.rect(display, (0, 255, 0), (0 - data.scrollX, 0 - data.scrollY, \
-                                            width, height))
 
-    # Draws bases
-    pygame.draw.ellipse(display, (0, 0, 0), (0 - baseRad - sx,
-                                             height - baseRad // 2 - sy,
-                                             baseRad * 2, baseRad))
-    pygame.draw.ellipse(display, (0, 0, 0), (width - baseRad - sx,
-                                             0 - baseRad // 2 - sy, baseRad * 2,
-                                             baseRad))
+	#print(grassImage.get_height())
 
-    # Draws blue towers
-    pygame.draw.ellipse(display, tower1Color, (margin - towerRad - sx,
-                                               height * 3 // 10 - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower1Color, (margin - towerRad - sx,
-                                               height // 2 - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower1Color, (width // 2 - towerRad - sx,
-                                               height - margin - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower1Color, (width * 7 // 10 - towerRad - sx,
-                                               height - margin - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower1Color,
-                        (width // 2 - 4 * margin - towerRad - sx,
-                         height // 2 + 4 * margin - towerRad // 2 - sy,
-                         towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower1Color, (width // 4 - towerRad - sx,
-                                               height * 3 // 4 - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
+	# Draw the whole map
+	#pygame.draw.rect(display, (0, 255, 0), (0 - data.scrollX, 0 - data.scrollY,
+										#width, height))
 
-    # Draws red towers
-    pygame.draw.ellipse(display, tower2Color, (width * 3 // 10 - towerRad - sx,
-                                               margin - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower2Color, (width // 2 - towerRad - sx,
-                                               margin - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower2Color, (width - margin - towerRad - sx,
-                                               height // 2 - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower2Color, (width - margin - towerRad - sx,
-                                               height * 7 // 10 - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower2Color,
-                        (width // 2 + 4 * margin - towerRad - sx,
-                         height // 2 - 4 * margin - towerRad // 2 - sy,
-                         towerRad * 2, towerRad))
-    pygame.draw.ellipse(display, tower2Color, (width * 3 // 4 - towerRad - sx,
-                                               height // 4 - towerRad // 2 - sy,
-                                               towerRad * 2, towerRad))
+
+
+
+	# Draws bases
+	pygame.draw.ellipse(display, (0, 0, 0), (0 - baseRad - sx,
+											 height - baseRad // 2 - sy,
+											 baseRad * 2, baseRad))
+	pygame.draw.ellipse(display, (0, 0, 0), (width - baseRad - sx,
+											 0 - baseRad // 2 - sy, baseRad * 2,
+											 baseRad))
+
+	# Draws blue towers
+	pygame.draw.ellipse(display, tower1Color, (margin - towerRad - sx,
+											   height * 3 // 10 - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower1Color, (margin - towerRad - sx,
+											   height // 2 - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower1Color, (width // 2 - towerRad - sx,
+											   height - margin - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower1Color, (width * 7 // 10 - towerRad - sx,
+											   height - margin - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower1Color,
+						(width // 2 - 4 * margin - towerRad - sx,
+						 height // 2 + 4 * margin - towerRad // 2 - sy,
+						 towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower1Color, (width // 4 - towerRad - sx,
+											   height * 3 // 4 - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+
+	# Draws red towers
+	pygame.draw.ellipse(display, tower2Color, (width * 3 // 10 - towerRad - sx,
+											   margin - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower2Color, (width // 2 - towerRad - sx,
+											   margin - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower2Color, (width - margin - towerRad - sx,
+											   height // 2 - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower2Color, (width - margin - towerRad - sx,
+											   height * 7 // 10 - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower2Color,
+						(width // 2 + 4 * margin - towerRad - sx,
+						 height // 2 - 4 * margin - towerRad // 2 - sy,
+						 towerRad * 2, towerRad))
+	pygame.draw.ellipse(display, tower2Color, (width * 3 // 4 - towerRad - sx,
+											   height // 4 - towerRad // 2 - sy,
+											   towerRad * 2, towerRad))
 
 
 def move(data, x, y):
-    # x, y is either 0 or 1 or -1
-    data.scrollX += x * data.mapStep
-    data.scrollY += y * data.mapStep
+	# x, y is either 0 or 1 or -1
+	data.scrollX += x * data.mapStep
+	data.scrollY += y * data.mapStep
+
+class backGround(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.transform.scale(pygame.image.load(
+			'sprites/grass.png'), (1000, 1000))
+
+def drawBoard(self, display):
+	backGround.__init__(self)
+
+	sx = self.scrollX
+	sy = self.scrollY
+
+	for i in range(7):
+		for j in range(7):
+			display.blit(self.image,
+						 (0 - sx + 1000 * i, 0 - sy + 1000 * j, 1000, 1000))
+
